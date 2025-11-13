745904860dab39b44bb29eb723d9c15e8708e849
KotelnikovaA <rabatenok@gmail.com>
2025-11-14 01:40:50 +0300

    first version of programm

---
+++ tasks/kotelnikova_a_num_sent_in_line/mpi/src/ops_mpi.cpp
@@ -0,0 +1,100 @@
+#include "kotelnikova_a_num_sent_in_line/mpi/include/ops_mpi.hpp"
+
+#include <mpi.h>
+
+#include <cctype>
+#include <string>
+
+#include "kotelnikova_a_num_sent_in_line/common/include/common.hpp"
+#include "util/include/util.hpp"
+
+namespace kotelnikova_a_num_sent_in_line {
+
+KotelnikovaANumSentInLineMPI::KotelnikovaANumSentInLineMPI(const InType &in) {
+  SetTypeOfTask(GetStaticTypeOfTask());
+  GetInput() = in;
+  GetOutput() = 0;
+}
+
+bool KotelnikovaANumSentInLineMPI::ValidationImpl() {
+  return true;
+}
+
+bool KotelnikovaANumSentInLineMPI::PreProcessingImpl() {
+  return true;
+}
+
+bool KotelnikovaANumSentInLineMPI::RunImpl() {
+  const std::string& text = GetInput();
+  int world_size, world_rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+  
+  int total_length = static_cast<int>(text.length());
+  
+  if (world_size == 1) {
+      GetOutput() = count_sentences_sequential(text);
+      return true;
+  }
+  
+  int chunk_size = total_length / world_size;
+  int remainder = total_length % world_size;
+  
+  int start = world_rank * chunk_size + std::min(world_rank, remainder);
+  int end = start + chunk_size + (world_rank < remainder ? 1 : 0);
+  
+  int local_count = 0;
+  bool has_unfinished_content = false;
+  
+  for (int i = start; i < end; ++i) {
+      char c = text[i];
+      
+      if (std::isalnum(static_cast<unsigned char>(c))) {
+          has_unfinished_content = true;
+      } else if ((c == '.' || c == '!' || c == '?') && has_unfinished_content) {
+          local_count++;
+          has_unfinished_content = false;
+      }
+  }
+  
+  int receives_unfinished = 0;
+  if (world_rank > 0) {
+      MPI_Recv(&receives_unfinished, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+  }
+  
+  if (receives_unfinished > 0) {
+      for (int i = start; i < std::min(start + 1, end); ++i) {
+          char c = text[i];
+          if ((c == '.' || c == '!' || c == '?') && receives_unfinished > 0) {
+              local_count++;
+              receives_unfinished = 0;
+              break;
+          }
+      }
+  }
+  
+  int sends_unfinished = has_unfinished_content ? 1 : 0;
+  if (world_rank < world_size - 1) {
+      MPI_Send(&sends_unfinished, 1, MPI_INT, world_rank + 1, 0, MPI_COMM_WORLD);
+  }
+  
+  int global_count = 0;
+  MPI_Reduce(&local_count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+  
+  if (world_rank == 0) {
+      GetOutput() = global_count;
+  }
+  
+  MPI_Bcast(&global_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  if (world_rank != 0) {
+      GetOutput() = global_count;
+  }
+  
+  return true;
+}
+
+bool KotelnikovaANumSentInLineMPI::PostProcessingImpl() {
+  return true;
+}
+
+}  // namespace kotelnikova_a_num_sent_in_line
